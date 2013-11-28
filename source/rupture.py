69e2c79386b8b76776f769cb55bc7a06ddf025d7
krisvanneste <kris.vanneste@oma.be>
2013-11-28 16:02:59 +0000

    Removed dependency of source.py in rupture.py
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/hazard/rshalib/trunk@1811 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- source/rupture.py
+++ source/rupture.py
@@ -4,7 +4,6 @@
 
 import openquake.hazardlib as oqhazlib
 
-from source import PointSource
 from ..geo import NodalPlane, Point
 from ..pmf import HypocentralDepthDistribution, NodalPlaneDistribution
 
@@ -12,12 +11,12 @@ from ..pmf import HypocentralDepthDistribution, NodalPlaneDistribution
 class Rupture(oqhazlib.source.Rupture):
 	"""
 	"""
-	
+
 	@classmethod
 	def from_hypocenter(cls, lon, lat, depth, mag, strike, dip, rake, trt, rms, rar, usd, lsd, msr):
 		"""
 		Create rupture by extending hypocenter with magnitude scaling relationship.
-		
+
 		:params lon:
 			float, lon of rupture hypocenter
 		:params lat:
@@ -44,7 +43,7 @@ class Rupture(oqhazlib.source.Rupture):
 			float, lower seismogenic depth
 		:param msr:
 			instance of subclass of :class:`openquake.hazardlib.scaler.BaseMSR`
-		
+
 		:return:
 			instance of :class:`Rupture`
 		"""
@@ -52,8 +51,8 @@ class Rupture(oqhazlib.source.Rupture):
 		nodal_plane = NodalPlane(strike, dip, rake)
 		npd = NodalPlaneDistribution([nodal_plane], [1.])
 		hdd = HypocentralDepthDistribution([depth], [1.])
-		point_source = PointSource("", "", trt, "", rms, msr, rar, usd, lsd, hypocenter, npd, hdd)
+		point_source = oqhazlib.source.PointSource("", "", trt, "", rms, msr, rar, usd, lsd, hypocenter, npd, hdd)
 		surface = point_source._get_rupture_surface(mag, nodal_plane, hypocenter)
-		rupture = cls(mag, rake, trt, hypocenter, surface, source_typology=PointSource)
+		rupture = cls(mag, rake, trt, hypocenter, surface, source_typology=oqhazlib.source.PointSource)
 		return rupture
 
