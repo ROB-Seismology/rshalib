face8b2217f8c87ecbc712a08f8345da0c17c82c
krisvanneste <kris.vanneste@oma.be>
2013-11-28 15:30:58 +0000

    Changed order of imported submodules in __init__.py Re-enabled reloading of config in openquake/__init__.py
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/hazard/rshalib/trunk@1810 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- __init__.py
+++ __init__.py
@@ -19,45 +19,72 @@ Models built with rshalib can be:
 	- exported to Openquake NRML format
 """
 
-import cav
-reload(cav)
+## Because of the reloads, the order of imported submodules is important,
+## as this may result in the following type of error:
 
+## super(OQ_Params, self).__init__(ini_filespec, configspec=configspec, write_empty_values=True, list_values=False)
+## TypeError: super(type, obj): obj must be an instance or subtype of type
+
+## Therefore, submodules should be loaded in order of inter-dependency,
+## i.e. a submodule depending on another one, should be loaded after that one.
+
+
+## No internal dependencies
+import nrml
+reload(nrml)
+
+## No internal dependencies
 import utils
 reload(utils)
 
+## No internal dependencies
+import cav
+reload(cav)
+
+## Depends on nrml
 import geo
 reload(geo)
 
+## Depends on geo, nrml
 import site
 reload(site)
 
-import pmf
-reload(pmf)
-
-import mfd
-reload(mfd)
-
-import source
-reload(source)
-
+## Depends on utils
 import gsim
 reload(gsim)
 
-import result
-reload(result)
+## Depends on geo, nrml, utils
+import pmf
+reload(pmf)
 
-import logictree
-reload(logictree)
+## Depends on nrml
+import mfd
+reload(mfd)
 
-import shamodel
-reload(shamodel)
+## Depends on geo, mfd, nrml
+import source
+reload(source)
 
+## Depends on geo, mfd, pmf, source
 import rob
 reload(rob)
 
+## Depends on nrml, pmf, site, utils
+import result
+reload(result)
+
+## Depends on mfd, result, source
 import crisis
 reload(crisis)
 
+## Depends on nrml, result, site
 import openquake
 reload(openquake)
 
+## Depends on nrml, pmf, source
+import logictree
+reload(logictree)
+
+## Depends on crisis, geo, gsim, logictree, openquake, pmf, result, site, source
+import shamodel
+reload(shamodel)
--- openquake/__init__.py
+++ openquake/__init__.py
@@ -9,7 +9,7 @@ import config
 ## Reloading config may in some cases result in the following error:
 ## super(OQ_Params, self).__init__(ini_filespec, configspec=configspec, write_empty_values=True, list_values=False)
 ## TypeError: super(type, obj): obj must be an instance or subtype of type
-#reload(config)
+reload(config)
 
 from IO import parse_hazard_curves, parse_hazard_map, parse_uh_spectra, parse_disaggregation
 
