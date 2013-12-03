858f1ebf6bfb80da50663ceed01439461afda1cf
krisvanneste <kris.vanneste@oma.be>
2013-12-03 16:16:31 +0000

    Change in write_crisis method of PSHAModelTree, to create subfolders for each GMPE if there is only 1 tectonig region type
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/hazard/rshalib/trunk@1831 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- shamodel/pshamodel.py
+++ shamodel/pshamodel.py
@@ -1075,7 +1075,8 @@ class PSHAModelTree(PSHAModelBase):
 		if not os.path.exists(gsims_dir):
 				os.mkdir(gsims_dir)
 
-		## Create directory structure for logic tree: only possible for source models
+		## Create directory structure for logic tree:
+		## only possible for source models
 		sm_filespecs = {}
 		all_filespecs = []
 		for source_model in self.source_models:
@@ -1083,10 +1084,19 @@ class PSHAModelTree(PSHAModelBase):
 			folder = os.path.join(self.output_dir, source_model.name)
 			if not os.path.exists(folder):
 				os.makedirs(folder)
+			## If there is only one TRT, it is possible to make subdirectories for each GMPE
+			trts = self.gmpe_lt.tectonicRegionTypes
+			if len(trts) == 1:
+				for gmpe_name in self.gmpe_lt.get_gmpe_names(trts[0]):
+					folder = os.path.join(folder, gmpe_name)
+					if not os.path.exists(folder):
+						os.makedirs(folder)
 
 		## Write CRISIS input files
 		for i, psha_model in enumerate(self.sample_logic_trees(self.num_lt_samples, verbose=verbose)):
 			folder = os.path.join(self.output_dir, psha_model.source_model.name)
+			if len(trts) == 1:
+				folder = os.path.join(folder, psha_model.ground_motion_model[trts[0]])
 			filespec = os.path.join(folder, 'lt-rlz-%04d.dat' % (i+1))
 			if os.path.exists(filespec) and overwrite:
 				os.unlink(filespec)
