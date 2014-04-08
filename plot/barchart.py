2610f36708d5d17d157d4eb6311c63d41be1d5ba
krisvanneste <kris.vanneste@oma.be>
2014-04-08 15:21:58 +0000

    Added plot_nested_variation_barchart function.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/hazard/rshalib/trunk@2239 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- plot/barchart.py
+++ plot/barchart.py
@@ -6,6 +6,18 @@ import pylab
 
 
 def plot_variation_barchart(mean_value, category_value_dict, ylabel, title):
+	"""
+	Plot simple barchart showing variations between different categories
+
+	:param mean_value:
+		float, mean value
+	:param category_value_dict:
+		dict, mapping category names to category values (lists)
+	:param ylabel:
+		str, label for Y axis
+	:param title
+		str, plot title
+	"""
 	category_names = category_value_dict.keys()
 	category_values = category_value_dict.values()
 	category_diffs = np.asarray(category_values) - mean_value
@@ -14,9 +26,6 @@ def plot_variation_barchart(mean_value, category_value_dict, ylabel, title):
 	bar_width = 0.35
 
 	rects = pylab.bar(xvalues, category_diffs, bar_width, color='r')
-	pylab.ylabel("$\Delta$ " + ylabel)
-	pylab.title(title)
-	pylab.xticks([])
 
 	def autolabel(rects, labels, values):
 		# attach some text labels
@@ -37,9 +46,80 @@ def plot_variation_barchart(mean_value, category_value_dict, ylabel, title):
 	ymax = category_diffs.max() + 0.2 * category_diffs.max()
 	pylab.hlines(0, xmin, xmax, lw=2, color='k')
 	pylab.text(xmax, 0, "%s" % mean_value, ha='left', va='center')
+	pylab.ylabel("$\Delta$ " + ylabel)
+	pylab.title(title)
+	pylab.xticks([])
 	pylab.axis((xmin, xmax, ymin, ymax))
+	pylab.grid(True)
 
 	pylab.show()
+
+
+def plot_nested_variation_barchart(mean_value, category_value_dict, ylabel, title):
+	"""
+	Plot barchart showing variations between different nested categories
+
+	:param mean_value:
+		float, mean value
+	:param category_value_dict:
+		dict, mapping category names to dictionaries mapping subcategory names
+		to subcategory values (lists)
+	:param ylabel:
+		str, label for Y axis
+	:param title
+		str, plot title
+	"""
+	category_names = category_value_dict.keys()
+	num_categories = len(category_names)
+	category_lengths = [len(category_value_dict[cat_name]) for cat_name in category_names]
+	xvalues, x0_values, yvalues, labels = [], [], [], []
+	for i, subcat_value_dict in enumerate(category_value_dict.values()):
+		try:
+			x0 = xvalues[-1] + 1
+		except:
+			x0 = 1
+		x0values.append(x0)
+		for j, (subcat_names, subcat_values) in enumerate(subcat_value_dict.items()):
+			subcat_len = len(subcat_names)
+			xvalues = np.concatenate([xvalues, np.add.accumulate(np.ones(subcat_len)) + x0])
+			yvalues.extend(subcat_values)
+			labels.extend(subcat.keys())
+
+	yvalues = np.array(yvalues)
+	diffs = yvalues - mean_value
+	bar_width = 0.5
+
+	rects = pylab.bar(xvalues, diffs, bar_width, color='r')
+
+	def autolabel(rects, labels, values):
+		# attach some text labels
+		for rect, label, value in zip(rects, labels, values):
+			x = rect.get_x() + rect.get_width() / 2.
+			if value < 0:
+				y = -rect.get_height() * 1.05
+				va = 'top'
+			else:
+				y = rect.get_height() * 1.05
+				va = 'bottom'
+
+			pylab.text(x, y, label, ha='center', va=va)
+
+	autolabel(rects, labels, diffs)
+	xmin, xmax = 0.5, xvalues[-1] + 1
+	ymin = diffs.min() + 0.2 * diffs.min()
+	ymax = diffs.max() + 0.2 * diffs.max()
+	pylab.hlines(0, xmin, xmax, lw=2, color='k')
+	pylab.vlines(x0values[1:], ymin, ymax, lw=2, ls='--', color='k')
+	pylab.text(xmax, 0, "%s" % mean_value, ha='left', va='center')
+	pylab.ylabel("$\Delta$ " + ylabel)
+	pylab.title(title)
+	pylab.xticks([])
+	pylab.axis((xmin, xmax, ymin, ymax))
+	pylab.grid(True)
+
+	pylab.show()
+
+
 
 
 
