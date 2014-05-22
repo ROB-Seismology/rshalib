7df83f5680c1c190d0acd1533f9c95d4361acac1
krisvanneste <kris.vanneste@oma.be>
2014-05-22 21:40:19 +0000

    Fixed another small bug in calc_shcf_mp method of DecomposedPSHAModelTree.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/hazard/rshalib/trunk@2463 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- shamodel/pshamodel.py
+++ shamodel/pshamodel.py
@@ -2952,7 +2952,7 @@ class DecomposedPSHAModelTree(PSHAModelTree):
 				print psha_model.name
 			# TODO: skip computation if output file already exists !
 			shcf_dict = psha_model.calc_shcf_mp(decompose_area_sources=True, num_cores=num_cores, combine_pga_and_sa=combine_pga_and_sa)
-			source_model_name, curve_name = psha_model.source_model.name.split('--')
+			source_model_name, curve_name = psha_model.source_model.name.split('--')[:2]
 			src = psha_model.source_model.sources[0]
 			trt = src.tectonic_region_type
 			gmpe_name = psha_model.ground_motion_model[trt]
