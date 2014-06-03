7631b9e14fcd2fb80df4321667f6913a46fd8764
krisvanneste <kris.vanneste@oma.be>
2014-06-03 11:58:16 +0000

    Another fix in get_oq_mean_sdc_by_source method of DecomposedPSHAModelTree.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/hazard/rshalib/trunk@2544 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- shamodel/pshamodel.py
+++ shamodel/pshamodel.py
@@ -3838,7 +3838,7 @@ class DecomposedPSHAModelTree(PSHAModelTree):
 				max_mag = self.source_model_lt.get_source_max_mag(source_model_name, src)
 				dmag = np.ceil((max_mag - min_mag) / sdc.mag_bin_width) * sdc.mag_bin_width
 				nmags = int(round(dmag / sdc.mag_bin_width))
-				mag_bins = min_mag + sdc.mag_bin_width * np.arange(nmags)
+				mag_bins = min_mag + sdc.mag_bin_width * np.arange(nmags + 1)
 				#mag_bins = sdc.mag_bin_width * np.arange(
 				#	int(np.floor(min_mag / sdc.mag_bin_width)),
 				#	int(np.ceil(max_mag / sdc.mag_bin_width) + 1)
