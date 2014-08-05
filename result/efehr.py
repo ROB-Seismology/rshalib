efeca171f2ba0099eeeff84c2bcef17d65655926
krisvanneste <kris.vanneste@oma.be>
2014-08-05 13:54:51 +0000

    Added several functions to query EFEHR web server.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/hazard/rshalib/trunk@2741 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- result/efehr.py
+++ result/efehr.py
@@ -4,13 +4,286 @@ Functions to download SHARE results from EFEHR website
 
 import os
 import urllib
+from lxml import etree
 from collections import OrderedDict
 
 import hazard.rshalib as rshalib
 
 
+def query_efehr(params, verbose=False):
+	"""
+	Query EFEHR website
 
-def get_hazard_map(region, period=0, poe=0.1, perc=None, verbose=True):
+	:params:
+		dict, mapping EFEHR URL parameters to values
+	:param verbose:
+		bool, prints url if set to True (default: False)
+
+	:return:
+		list with lines of data returned by the server
+	"""
+	## Construct URL
+	base_url = "http://appsrvr.share-eu.org:8080/share/map"
+	url = "%s?%s" % (base_url, urllib.urlencode(params))
+	if verbose:
+		print url
+
+	f = urllib.urlopen(url)
+	data = f.readlines()
+	f.close()
+
+	return data
+
+
+def get_map_models(lon, lat, verbose=False):
+	"""
+	Provide an index of all hazard models that are defined for a specific point of interest.
+
+	:param lon:
+		float, longitude
+	:param lat:
+		float, latitude
+	:param verbose:
+		bool, prints url if set to True (default: False)
+
+	:return:
+		dict mapping map id's to map names
+	"""
+	efehr_params = OrderedDict()
+
+	efehr_params['lon'] = lon
+	efehr_params['lat'] = lat
+
+	xml_data = query_efehr(efehr_params, verbose=verbose)
+	xml = etree.fromstring(xml_data[0])
+
+	map_models = OrderedDict()
+	for mdl_elem in xml.findall('model'):
+		id = int(mdl_elem.find('id').text)
+		name = mdl_elem.find('name').text
+		map_models[id] = name
+
+	return map_models
+
+
+def get_map_imts(map_id, verbose=False):
+	"""
+	Provide all intensity measurement types (IMT) for which a specified model
+	provides hazard information.
+
+	:param map_id:
+		int, map id
+	:param verbose:
+		bool, prints url if set to True (default: False)
+
+	:return:
+		list with (imt string, imt unit) tuples
+	"""
+	efehr_params = OrderedDict()
+
+	efehr_params['id'] = map_id
+
+	xml_data = query_efehr(efehr_params, verbose=verbose)
+	xml = etree.fromstring(xml_data[0])
+
+	imts = []
+	for imt_elem in xml.findall('imtcode'):
+		imt = imt_elem.find('code').text
+		imt_unit = imt_elem.find('imunitdescr').text
+		imts.append((imt, imt_unit))
+
+	return imts
+
+
+def get_map_poes(map_id, imt_string, verbose=False):
+	"""
+	Provide probability of exceedance and investigation timespan's with maps
+	available, given model ID and intensity measurement types (IMT) for which
+	a specified model provides hazard information.
+
+	:param map_id:
+		int, map id
+	:param imt_string:
+		str, imt string
+	:param verbose:
+		bool, prints url if set to True (default: False)
+
+	:return:
+		list with (poe, timespan) tuples
+	"""
+	efehr_params = OrderedDict()
+
+	efehr_params['id'] = map_id
+	efehr_params['imt'] = imt_string
+
+	xml_data = query_efehr(efehr_params, verbose=verbose)
+	xml = etree.fromstring(xml_data[0])
+
+	poes = []
+	for poe_elem in xml.findall('exceedance'):
+		poe = float(poe_elem.find('hmapexceedprob').text)
+		timespan = int(poe_elem.find('hmapexceedyears').text)
+		poes.append((poe, timespan))
+
+	return poes
+
+
+def get_map_soil_classes(map_id, imt_string, poe, timespan, verbose=False):
+	"""
+	Provide site class with maps available, given model ID, IMT, POE, time span
+	for which a specified model provides hazard information.
+
+	:param map_id:
+		int, map id
+	:param imt_string:
+		str, imt string
+	:param poe:
+		float, probability of exceedance
+	:param timespan:
+		int, investigation timespan
+	:param verbose:
+		bool, prints url if set to True (default: False)
+
+	:return:
+		list with soil classes
+	"""
+	efehr_params = OrderedDict()
+
+	efehr_params['id'] = map_id
+	efehr_params['imt'] = imt_string
+	efehr_params['hmapexceedprob'] = poe
+	efehr_params['hmapexceedyears'] = timespan
+
+	xml_data = query_efehr(efehr_params, verbose=verbose)
+	xml = etree.fromstring('<soiltypes>' + xml_data[0] + '</soiltypes>')
+
+	soil_classes = []
+	for soil_elem in xml.findall('soiltype'):
+		soil_class = soil_elem.find('type').text
+		soil_classes.append(soil_class)
+
+	return soil_classes
+
+
+def get_map_aggregation_types(map_id, imt_string, poe, timespan, soil_class, verbose=False):
+	"""
+	Provide aggregation types with maps available, given model ID, IMT, POE,
+	time span, site class for which a specified model provides hazard information.
+
+	:param map_id:
+		int, map id
+	:param imt_string:
+		str, imt string
+	:param poe:
+		float, probability of exceedance
+	:param timespan:
+		int, investigation timespan
+	:param soil_class:
+		str, soil class
+	:param verbose:
+		bool, prints url if set to True (default: False)
+
+	:return:
+		list with (aggregation type, aggregation level) tuples
+	"""
+	efehr_params = OrderedDict()
+
+	efehr_params['id'] = map_id
+	efehr_params['imt'] = imt_string
+	efehr_params['hmapexceedprob'] = poe
+	efehr_params['hmapexceedyears'] = timespan
+	efehr_params['soiltype'] = soil_class
+
+	xml_data = query_efehr(efehr_params, verbose=verbose)
+	xml = etree.fromstring(xml_data[0])
+
+	aggregation_types = []
+	for fractile_elem in xml.findall('fractile'):
+		agg_type = fractile_elem.find('aggregationtype').text
+		agg_level = float(fractile_elem.find('aggregationlevel').text)
+		aggregation_types.append((agg_type, agg_level))
+
+	return aggregation_types
+
+
+def get_map_wms_id(map_id, imt_string, poe, timespan, soil_class, agg_type, agg_level, verbose=False):
+	"""
+	Provide the map identity, and the layer reference for the web map service,
+	of the hazard map for given model ID, IMT, POE, time span, site class and
+	aggregation type.
+
+	:param map_id:
+		int, map id
+	:param imt_string:
+		str, imt string
+	:param poe:
+		float, probability of exceedance
+	:param timespan:
+		int, investigation timespan
+	:param soil_class:
+		str, soil class
+	:param agg_type:
+		str, aggregation type
+	:param agg_level:
+		float, aggregation level
+	:param verbose:
+		bool, prints url if set to True (default: False)
+
+	:return:
+		tuple of (WMS id, WMS name)
+	"""
+	efehr_params = OrderedDict()
+
+	efehr_params['id'] = map_id
+	efehr_params['imt'] = imt_string
+	efehr_params['hmapexceedprob'] = poe
+	efehr_params['hmapexceedyears'] = timespan
+	efehr_params['soiltype'] = soil_class
+	efehr_params['aggregationtype'] = agg_type
+	efehr_params['aggregationlevel'] = agg_level
+
+	xml_data = query_efehr(efehr_params, verbose=verbose)
+	xml = etree.fromstring(xml_data[0])
+
+	wms_id = int(xml.find('hmapid').text)
+	wms_name = xml.find('hmapwms').text
+
+	return (wms_id, wms_name)
+
+
+def get_map_wms_url(map_id, imt_string, poe, timespan, soil_class, agg_type, agg_level, verbose=False):
+	"""
+	Provide the URL for the web map service for given model ID, IMT, POE,
+	time span, site class and aggregation type.
+
+	:param map_id:
+		int, map id
+	:param imt_string:
+		str, imt string
+	:param poe:
+		float, probability of exceedance
+	:param timespan:
+		int, investigation timespan
+	:param soil_class:
+		str, soil class
+	:param agg_type:
+		str, aggregation type
+	:param agg_level:
+		float, aggregation level
+	:param verbose:
+		bool, prints url if set to True (default: False)
+
+	:return:
+		int, WMS id
+	"""
+	wms_id, wms_name = get_map_wms_id(map_id, imt_string, poe, timespan, soil_class, agg_type, agg_level, verbose=verbose)
+
+	#url = 'http://efehrmaps.ethz.ch/cgi-bin/mapserv?MAP=/var/www/mapfile/sharehazard.01.map&LAYERS=%s&SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap' % wms_name
+	url = 'http://gemmsrvr.ethz.ch/cgi-bin/mapserv?MAP=/var/www/mapfile/sharehazard.01.map&LAYERS=%s&SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap' % wms_name
+	return url
+
+
+def get_hazard_map(region, period=0, poe=0.1, perc=None, verbose=False):
 	"""
 	Fetch hazard map
 
@@ -26,7 +299,7 @@ def get_hazard_map(region, period=0, poe=0.1, perc=None, verbose=True):
 		float, percentile (None, 5, 15, 50, 85, 95)
 		If None, arithmetic mean will be selected
 	:param verbose:
-		bool, prints url if set to True (default: True)
+		bool, prints url if set to True (default: False)
 
 	:return:
 		instance of :class:`rshalib.result.HazardMap`
@@ -61,15 +334,7 @@ def get_hazard_map(region, period=0, poe=0.1, perc=None, verbose=True):
 		efehr_params['aggregationtype'] = "ordinal"
 		efehr_params['aggregationlevel'] = perc / 100.
 
-	## Construct URL
-	base_url = "http://appsrvr.share-eu.org:8080/share/map"
-	url = "%s?%s" % (base_url, urllib.urlencode(efehr_params))
-	print url
-
-	f = urllib.urlopen(url)
-	data = f.readlines()
-	f.close()
-
+	data = query_efehr(efehr_params, verbose=verbose)
 
 	model_name = "%s - %s - %s" % (hazard_model, soil_type, efehr_params['imt'])
 	if perc is None:
@@ -104,10 +369,28 @@ def get_hazard_spectrum():
 
 
 if __name__ == "__main__":
+	lon, lat = 4, 51
+	map_models = get_map_models(lon, lat)
+	for map_id, map_name in map_models.items():
+		print map_id,map_name
+		for imt, imt_unit in get_map_imts(map_id):
+			print imt, imt_unit
+			for poe, timespan in get_map_poes(map_id, imt):
+				print poe, timespan
+				for soil_class in get_map_soil_classes(map_id, imt, poe, timespan):
+					print soil_class
+					for agg_type, agg_level in get_map_aggregation_types(map_id, imt, poe, timespan, soil_class):
+						print agg_type, agg_level
+						#print get_map_wms_id(map_id, imt, poe, timespan, soil_class, agg_type, agg_level)
+						print get_map_wms_url(map_id, imt, poe, timespan, soil_class, agg_type, agg_level)
+
+
+	"""
 	region = (2,7,49.25,51.75)
 	#region = (4,5,50,51)
 	period = 0
 	poe = 0.1
-	hm = get_hazard_map(region, period=period, poe=poe, verbose=False)
+	hm = get_hazard_map(region, period=period, poe=poe, verbose=True)
 	map = hm.get_plot()
 	map.plot()
+	"""
