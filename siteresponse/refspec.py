2949349756c4d36fd4f62dfb805030de9a8092f8
krisvanneste <kris.vanneste@oma.be>
2015-04-30 12:23:38 +0000

    Added orientation parameter to get_refspec_EC8 function.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/hazard/rshalib/trunk@3169 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- siteresponse/refspec.py
+++ siteresponse/refspec.py
@@ -39,7 +39,7 @@ def get_refspec_RG160(ag, damping=5):
 	return ResponseSpectrum("RG1.60", periods, "SA", values, intensity_unit="g")
 
 
-def get_refspec_EC8(ag, ground_type, resp_type, damping=5):
+def get_refspec_EC8(ag, ground_type, resp_type, orientation="horizontal", damping=5):
 	"""
 	Reference horizontal elastic response spectrum according to Eurocode 8
 	(if deep geology is not accounted for)
@@ -54,6 +54,9 @@ def get_refspec_EC8(ag, ground_type, resp_type, damping=5):
 			have MS >= 5.5
 		2: if earthquakes contributing most to seismic hazard for the site
 			have MS < 5.5
+	:param orientation:
+		str, either "horizontal" or "vertical", orientation of spectrum
+		(default: "horizontal")
 	:param damping:
 		float, viscous damping, in % (default: 5)
 
@@ -62,32 +65,57 @@ def get_refspec_EC8(ag, ground_type, resp_type, damping=5):
 	"""
 	ground_type = ground_type.upper()
 	if resp_type == 1:
-		S = {'A': 1.0, 'B': 1.2, 'C': 1.15, 'D': 1.35, 'E': 1.4}[ground_type]
-		TB = {'A': 0.15, 'B': 0.15, 'C': 0.2, 'D': 0.2, 'E': 0.15}[ground_type]
-		TC = {'A': 0.4, 'B': 0.5, 'C': 0.6, 'D': 0.8, 'E': 0.5}[ground_type]
-		TD = {'A': 2.0, 'B': 2.0, 'C': 2.0, 'D': 2.0, 'E': 2.0}[ground_type]
+		if orientation == "horizontal":
+			S = {'A': 1.0, 'B': 1.2, 'C': 1.15, 'D': 1.35, 'E': 1.4}[ground_type]
+			TB = {'A': 0.15, 'B': 0.15, 'C': 0.2, 'D': 0.2, 'E': 0.15}[ground_type]
+			TC = {'A': 0.4, 'B': 0.5, 'C': 0.6, 'D': 0.8, 'E': 0.5}[ground_type]
+			TD = {'A': 2.0, 'B': 2.0, 'C': 2.0, 'D': 2.0, 'E': 2.0}[ground_type]
+		elif orientation == "vertical":
+			vh_ratio = 0.9
+			TB, TC, TD = 0.05, 0.15, 1.  ## Is this correct for type 1 ??
 	elif resp_type == 2:
-		S = {'A': 1.0, 'B': 1.35, 'C': 1.5, 'D': 1.8, 'E': 1.6}[ground_type]
-		TB = {'A': 0.05, 'B': 0.05, 'C': 0.1, 'D': 0.1, 'E': 0.05}[ground_type]
-		TC = {'A': 0.25, 'B': 0.25, 'C': 0.25, 'D': 0.30, 'E': 0.25}[ground_type]
-		TD = {'A': 1.2, 'B': 1.2, 'C': 1.2, 'D': 1.2, 'E': 1.2}[ground_type]
+		if orientation == "horizontal":
+			S = {'A': 1.0, 'B': 1.35, 'C': 1.5, 'D': 1.8, 'E': 1.6}[ground_type]
+			TB = {'A': 0.05, 'B': 0.05, 'C': 0.1, 'D': 0.1, 'E': 0.05}[ground_type]
+			TC = {'A': 0.25, 'B': 0.25, 'C': 0.25, 'D': 0.30, 'E': 0.25}[ground_type]
+			TD = {'A': 1.2, 'B': 1.2, 'C': 1.2, 'D': 1.2, 'E': 1.2}[ground_type]
+		elif orientation == "vertical":
+			vh_ratio = 0.45
+			TB, TC, TD = 0.05, 0.15, 1.
+
 
 	eta = np.sqrt(10.0 / (5 + damping))
 
 	periods = np.concatenate([[0.0] + np.arange(0.04, 4.02, 0.02)])
 	values = np.zeros(len(periods), 'd')
 
-	for i, T in enumerate(periods):
-		if 0 <= T < TB:
-			values[i] = ag * S * (1 + (T/TB)*(eta * 2.5 - 1))
-		elif TB <= T < TC:
-			values[i] = ag * S * eta * 2.5
-		elif TC <= T < TD:
-			values[i] = ag * S * eta * 2.5 * (TC/T)
-		elif T >= TD:
-			values[i] = ag * S * eta * 2.5 * ((TC*TD)/T**2)
+	if orientation == "horizontal":
+		for i, T in enumerate(periods):
+			if 0 <= T < TB:
+				values[i] = ag * S * (1 + (T/TB)*(eta * 2.5 - 1))
+			elif TB <= T < TC:
+				values[i] = ag * S * eta * 2.5
+			elif TC <= T < TD:
+				values[i] = ag * S * eta * 2.5 * (TC/T)
+			elif T >= TD:
+				values[i] = ag * S * eta * 2.5 * ((TC*TD)/T**2)
+	elif orientation == "vertical":
+		assert ground_type.upper() == "A"
+		avg = ag * vh_ratio
+		for i, T in enumerate(periods):
+			if 0 <= T < TB:
+				values[i] = avg * (1 + (T/TB)*(eta * 3 - 1))
+			elif TB <= T < TC:
+				values[i] = avg * eta * 3
+			elif TC <= T < TD:
+				values[i] = avg * eta * 3 * (TC/T)
+			elif T >= TD:
+				values[i] = avg * eta * 3 * ((TC*TD)/T**2)
 
-	model_name = "EC8 - class %s, type %d" % (ground_type, resp_type)
+	if orientation == "horizontal":
+		model_name = "EC8 - class %s, type %d" % (ground_type, resp_type)
+	else:
+		model_name = "EC8 type %d, vertical" % resp_type
 	return ResponseSpectrum(model_name, periods, "SA", values, intensity_unit="g")
 
 
