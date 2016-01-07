c6eeeba676d516fb9fa0f9afc08c1ce6f2dbd142
krisvanneste <kris.vanneste@oma.be>
2016-01-07 07:58:33 +0000

    Fixed bug in __init__ method of PointSource, AreaSource, SimpleFaultSource and ComplexFaultSource, related to magnitude_scaling_relationship passed as string.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/hazard/rshalib/trunk@3525 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- source/source.py
+++ source/source.py
@@ -383,7 +383,7 @@ class PointSource(oqhazlib.source.PointSource, RuptureSource):
 	"""
 	def __init__(self, source_id, name, tectonic_region_type, mfd, rupture_mesh_spacing, magnitude_scaling_relationship, rupture_aspect_ratio, upper_seismogenic_depth, lower_seismogenic_depth, location, nodal_plane_distribution, hypocenter_distribution):
 		if isinstance(magnitude_scaling_relationship, (str, unicode)):
-			magnitude_scaling_relationship = getattr(oqhazlib.scalerel, magnitude_scaling_relationship)
+			magnitude_scaling_relationship = getattr(oqhazlib.scalerel, magnitude_scaling_relationship)()
 		super(PointSource, self).__init__(source_id=source_id,
 				name=name,
 				tectonic_region_type=tectonic_region_type,
@@ -606,7 +606,7 @@ class AreaSource(oqhazlib.source.AreaSource, RuptureSource):
 
 	def __init__(self, source_id, name, tectonic_region_type, mfd, rupture_mesh_spacing, magnitude_scaling_relationship, rupture_aspect_ratio, upper_seismogenic_depth, lower_seismogenic_depth, nodal_plane_distribution, hypocenter_distribution, polygon, area_discretization):
 		if isinstance(magnitude_scaling_relationship, (str, unicode)):
-			magnitude_scaling_relationship = getattr(oqhazlib.scalerel, magnitude_scaling_relationship)
+			magnitude_scaling_relationship = getattr(oqhazlib.scalerel, magnitude_scaling_relationship)()
 		super(AreaSource, self).__init__(source_id=source_id,
 				name=name,
 				tectonic_region_type=tectonic_region_type,
@@ -948,7 +948,7 @@ class SimpleFaultSource(oqhazlib.source.SimpleFaultSource, RuptureSource):
 	# TODO: add aseismic_coef and strain_drop parameters (see get_MFD_Anderson_Luco method)
 	def __init__(self, source_id, name, tectonic_region_type, mfd, rupture_mesh_spacing, magnitude_scaling_relationship, rupture_aspect_ratio, upper_seismogenic_depth, lower_seismogenic_depth, fault_trace, dip, rake, slip_rate=numpy.NaN, bg_zone=None):
 		if isinstance(magnitude_scaling_relationship, (str, unicode)):
-			magnitude_scaling_relationship = getattr(oqhazlib.scalerel, magnitude_scaling_relationship)
+			magnitude_scaling_relationship = getattr(oqhazlib.scalerel, magnitude_scaling_relationship)()
 		super(SimpleFaultSource, self).__init__(source_id=source_id,
 				name=name,
 				tectonic_region_type=tectonic_region_type,
@@ -1575,7 +1575,7 @@ class ComplexFaultSource(oqhazlib.source.ComplexFaultSource, RuptureSource):
 	# TODO: add bg_zone parameter as for SimpleFaultSource
 	def __init__(self, source_id, name, tectonic_region_type, mfd, rupture_mesh_spacing, magnitude_scaling_relationship, rupture_aspect_ratio, edges, rake):
 		if isinstance(magnitude_scaling_relationship, (str, unicode)):
-			magnitude_scaling_relationship = getattr(oqhazlib.scalerel, magnitude_scaling_relationship)
+			magnitude_scaling_relationship = getattr(oqhazlib.scalerel, magnitude_scaling_relationship)()
 		super(ComplexFaultSource, self).__init__(source_id=source_id,
 				name=name,
 				tectonic_region_type=tectonic_region_type,
