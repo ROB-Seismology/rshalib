30871beaf8df93a0c59d279e0040be458c86e84f
krisvanneste <kris.vanneste@oma.be>
2018-07-12 19:20:39 +0000

    Added import of NhlibGMPE in __init__.py.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/hazard/rshalib/trunk@4716 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- gsim/__init__.py
+++ gsim/__init__.py
@@ -21,6 +21,6 @@ from gmpe import (CRISIS_DistanceMetrics, sd2psa, sd2psv, GMPE, AmbraseysEtAl199
 	BindiEtAl2011, BooreAtkinson2008, BooreAtkinson2008Prime, Campbell2003, Campbell2003SHARE, Campbell2003adjusted,
 	CauzziFaccioli2008, ChiouYoungs2008, FaccioliEtAl2010, FaccioliEtAl2010Ext, ToroEtAl2002,
 	ToroEtAl2002SHARE, ToroEtAl2002adjusted, ZhaoEtAl2006Asc, RietbrockEtAl2013SS, RietbrockEtAl2013MD,
-	Campbell2003HTT, ToroEtAl2002HTT, RietbrockEtAl2013MDHTT,
+	Campbell2003HTT, ToroEtAl2002HTT, RietbrockEtAl2013MDHTT, NhlibGMPE,
 	PezeshkEtAl2011, Anbazhagan2013, adjust_hard_rock_to_rock, adjust_host_to_target,
 	adjust_faulting_style, adjust_components, plot_distance, plot_spectrum)
