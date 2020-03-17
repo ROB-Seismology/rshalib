b7cdb7cd394d3b6cec45e9a0947537c3d73c11b4
krisvanneste <kris.vanneste@oma.be>
2020-03-17 14:56:32 +0000

    PY3 compatibility changes.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/hazard/rshalib/trunk@6176 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- nrml/common.py
+++ nrml/common.py
@@ -2,40 +2,83 @@
 Common constants and functions used to generate NRML
 """
 
+# Note: do not import unicode_literals here!
+from __future__ import absolute_import, division, print_function
+
+import sys
 from lxml import etree
-import ns
 
 
-ENUM_OQ_UNCERTAINTYTYPES = set(("gmpeModel", "sourceModel", "maxMagGRAbsolute", "maxMagGRRelative", "abGRAbsolute", "bGRRelative", "incrementalMFDRates"))
-ENUM_OQ_TRT = set(('Active Shallow Crust', 'Stable Shallow Crust', 'Subduction Interface', 'Subduction IntraSlab', 'Volcanic'))
+if sys.version_info.major == 2:
+	PY2 = True
+else:
+	PY2 = False
+	basestring = str
+	unicode = str
+
+
+
+__all__ = ['ENUM_OQ_UNCERTAINTYTYPES', 'ENUM_OQ_TRT', 'INVALID_XML_CHARS',
+			'xmlstr', 'create_nrml_root', 'NRMLError']
+
+
+ENUM_OQ_UNCERTAINTYTYPES = set(("gmpeModel",
+								"sourceModel",
+								"maxMagGRAbsolute",
+								"maxMagGRRelative",
+								"abGRAbsolute",
+								"bGRRelative",
+								"incrementalMFDRates"))
+
+ENUM_OQ_TRT = set(('Active Shallow Crust',
+					'Stable Shallow Crust',
+					'Subduction Interface',
+					'Subduction IntraSlab',
+					'Volcanic'))
 
 INVALID_XML_CHARS = ['&', '<', '>', '"', "'"]
 
 
 def xmlstr(s, encoding='latin1'):
 	"""
-	Generate XML-readable string, making sure unicode characters or codes are
-	replaced with xml characters
-	Arguments:
-		s: any object that can be represented as a string
-		encoding: unicode encoding if s is a string (default: 'latin1')
+	Generate XML-readable string, making sure unicode characters
+	or codes are replaced with xml characters
+
+	:param s:
+		any object that can be represented as a string
+	:param encoding:
+		unicode encoding if s is not unicode
+		(default: 'latin1')
+
+	:return:
+		str
 	"""
-	for char in INVALID_XML_CHARS:
-		s = s.replace(char, '')
-	if isinstance(s, str):
-		return s.decode(encoding).encode('ascii', 'xmlcharrefreplace')
-	elif isinstance(s, unicode):
+	if isinstance(s, basestring):
+		for char in INVALID_XML_CHARS:
+			s = s.replace(char, '')
+	if not isinstance(s, unicode):
+		s = s.decode(encoding)
+	if isinstance(s, unicode):
 		return s.encode('ascii', 'xmlcharrefreplace')
 	else:
-		return str(s)
+		return bytes(s)
 
 
 def create_nrml_root(xml_elem, encoding='latin1', **kwargs):
 	"""
 	Create nrml root
-	Arguments:
-		encoding: unicode encoding (default: 'latin1')
+
+	:param xml_elem:
+		instance of class having a `create_xml_element` method
+	:param encoding:
+		unicode encoding
+		(default: 'latin1')
+
+	:return:
+		instance of :class:`etree.ElementTree`
 	"""
+	from . import ns
+
 	nrml_elem = etree.Element(ns.ROOT, nsmap=ns.NSMAP)
 	nrml_elem.append(xml_elem.create_xml_element(encoding=encoding, **kwargs))
 
@@ -46,5 +89,4 @@ def create_nrml_root(xml_elem, encoding='latin1', **kwargs):
 
 class NRMLError(Exception):
 	pass
-
 
