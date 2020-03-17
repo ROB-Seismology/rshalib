25e29f465e6e8a17f189a656c0ab66e1b55d6f82
krisvanneste <kris.vanneste@oma.be>
2020-03-17 14:49:19 +0000

    Improved reloading mechanism.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/hazard/rshalib/trunk@6174 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- nrml/__init__.py
+++ nrml/__init__.py
@@ -1,11 +1,35 @@
-#
-# Empty file necessary for python to recognise directory as package
-#
+"""
+nrml submodule
+"""
 
-import ns
-reload(ns)
+from __future__ import absolute_import, division, print_function, unicode_literals
 
-import common
-reload(common)
 
-from common import create_nrml_root, xmlstr
+
+## Reloading mechanism
+try:
+	reloading
+except NameError:
+	## Module is imported for the first time
+	reloading = False
+else:
+	## Module is reloaded
+	reloading = True
+	try:
+		## Python 3
+		from importlib import reload
+	except ImportError:
+		## Python 2
+		pass
+
+
+if not reloading:
+	from . import ns
+else:
+	reload(ns)
+
+if not reloading:
+	from . import common
+else:
+	reload(common)
+from .common import *
