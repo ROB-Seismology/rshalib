e45d7d34b864969366f7786542032307526b89bd
krisvanneste <kris.vanneste@oma.be>
2020-03-18 22:53:35 +0000

    Improved reloading mechanism.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/hazard/rshalib/trunk@6190 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- site/__init__.py
+++ site/__init__.py
@@ -1,13 +1,44 @@
-#
-# Empty file necessary for python to recognise directory as package
-#
+"""
+site submodule
+"""
 
-import ref_soil_params
-reload(ref_soil_params)
+from __future__ import absolute_import, division, print_function, unicode_literals
 
-import site
-reload(site)
 
-from ref_soil_params import REF_SOIL_PARAMS
-from site import SHASite, SHASiteModel, SoilSite, SoilSiteModel
 
+## Reloading mechanism
+try:
+	reloading
+except NameError:
+	## Module is imported for the first time
+	reloading = False
+else:
+	## Module is reloaded
+	reloading = True
+	try:
+		## Python 3
+		from importlib import reload
+	except ImportError:
+		## Python 2
+		pass
+
+
+## vs30
+if not reloading:
+	from . import vs30
+else:
+	reload(vs30)
+
+## ref_soil_params (depends on vs30)
+if not reloading:
+	from . import ref_soil_params
+else:
+	reload(ref_soil_params)
+from .ref_soil_params import REF_SOIL_PARAMS
+
+## site (depends on ref_soil_params)
+if not reloading:
+	from . import site
+else:
+	reload(site)
+from .site import *
