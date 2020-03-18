7c37cc61ecea0049cd64beca74b8c4c0a804dc8e
krisvanneste <kris.vanneste@oma.be>
2020-03-18 09:40:53 +0000

    Improved reloading mechanism.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/hazard/rshalib/trunk@6185 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- calc/__init__.py
+++ calc/__init__.py
@@ -1,6 +1,28 @@
-#
-# Empty file necessary for python to recognise directory as package
-#
+"""
+calc submodule (multiprocessing using python standard library)
+"""
 
-import mp
-reload(mp)
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+
+
+## Reloading mechanism
+try:
+	reloading
+except NameError:
+	## Module is imported for the first time
+	reloading = False
+else:
+	## Module is reloaded
+	reloading = True
+	try:
+		## Python 3
+		from importlib import reload
+	except ImportError:
+		## Python 2
+		pass
+
+if not reloading:
+	from . import mp
+else:
+	reload(mp)
