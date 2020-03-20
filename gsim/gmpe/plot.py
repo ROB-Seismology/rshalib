de4261d1c5a6b9d3f4922d159124d7a1b1b8e3b4
krisvanneste <kris.vanneste@oma.be>
2020-03-20 14:39:52 +0000

    Reimplemented plot_distance function using generic_mpl library.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/hazard/rshalib/trunk@6203 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- gsim/gmpe/plot.py
+++ gsim/gmpe/plot.py
@@ -9,6 +9,8 @@ import numpy as np
 import pylab
 from matplotlib.font_manager import FontProperties
 
+from plotting.generic_mpl import plot_xy
+
 from ...utils import interpolate, logrange
 
 
@@ -16,140 +18,158 @@ __all__ = ['plot_distance', 'plot_spectrum']
 
 
 def plot_distance(gmpe_list, mags, dmin=None, dmax=None, distance_metric=None,
-				h=0, imt="PGA", T=0, imt_unit="g", epsilon=0, soil_type="rock",
-				vs30=None, kappa=None, mechanism="normal", damping=5,
-				plot_style="loglog", amin=None, amax=None, colors=None,
-				fig_filespec=None, title="", want_minor_grid=False,
-				legend_location=0, lang="en"):
+				h=0, imt='PGA', T=0, imt_unit='g', epsilon=0, soil_type='rock',
+				vs30=None, kappa=None, mechanism='normal', damping=5,
+				xscaling='log', yscaling='log', xgrid=1, ygrid=1,
+				colors=None, linestyles=None, linewidths=None, legend_location=0,
+				lang='en', title='', fig_filespec=None, **kwargs):
 	"""
 	Function to plot ground motion versus distance for one or more GMPE's.
 	Horizontal axis: distances.
 	Vertical axis: ground motion values.
 
 	:param gmpe_list:
-		list of GMPE objects.
+		list with instances of :class:`GMPE`
 	:param mags:
 		list of floats, magnitudes to plot
 	:param dmin:
 		float, lower distance in km. If None, use the lower bound of the
 		distance range of each GMPE
-		(default: None).
+		(default: None)
 	:param dmax:
 		float, upper distance in km. If None, use the lower bound of the
 		valid distance range of each GMPE
-		(default: None).
+		(default: None)
 	:param distance_metric:
 		str, distance_metric to plot (options: "Joyner-Boore", "Rupture")
 		(default: None, distance metrics of gmpes are used)
 	:param h:
 		float, depth in km. Ignored if distance metric of GMPE is
 		epicentral or Joyner-Boore
-		(default: 0).
+		(default: 0)
 	:param imt:
 		str, one of the supported intensity measure types.
-		(default: "PGA").
+		(default: 'PGA')
 	:param T:
-		float, period to plot
+		float, spectral period to plot if imt is not peak-ground motion
 		(default: 0).
 	:param imt_unit:
 		str, unit in which intensities should be expressed,
 		depends on IMT
-		(default: "g")
+		(default: 'g')
 	:param epsilon:
 		float, number of standard deviations above or below the mean to
 		plot in addition to the mean
-		(default: 0).
+		(default: 0)
 	:param soil_type:
 		str, one of the soil types supported by the particular GMPE
-		(default: "rock").
+		(default: "rock")
 	:param vs30:
 		float, shear-wave velocity in the upper 30 m (in m/s).
-		If not None, it takes precedence over the soil_type parameter
-		(default: None).
+		If not None, it takes precedence over :param:`soil_type`
+		(default: None)
 	:param kappa:
 		float, kappa value, in seconds
 		(default: None)
 	:param mechanism:
 		str, fault mechanism: either "normal", "reverse" or "strike-slip"
-		(default: "normal").
+		(default: 'normal')
 	:param damping:
 		float, damping in percent
-		(default: 5).
-	:param plot_style:
-		str, plotting style ("lin", "loglin", "linlog" or "loglog").
-		First term refers to horizontal axis, second term to vertical axis.
-		(default: "loglog").
-	:param amin:
-		float, lower ground-motion value to plot
-		(default: None).
-	:param amax:
-		upper ground-motion value to plot
-		(default: None).
+		(default: 5)
+	:param xscaling:
+		str, scaling to use for X axis ('lin' or 'log')
+		Prepend '-' to invert orientation of X axis
+		(default: 'log')
+	:param yscaling:
+		cf. :param:`xscaling`, but for Y axis
+		(default: 'log')
+	:param xgrid:
+		int, 0/1/2/3 = draw no/major/minor/major+minor X grid lines
+		(default: 1)
+	:param ygrid:
+		cf. :param:`xgrid`, but for Y axis
+		(default: 1)
 	:param colors:
-		List of matplotlib color specifications
+		list of matplotlib color specifications for each GMPE
+		(default: None, will use default colors)
+	:param linestyles:
+		list of strings, matplotlib linestyle specifications for each
+		magnitude
 		(default: None)
-	:param fig_filespec:
-		str, full path specification of output file
-		(default: None).
-	:param title:
-		str, plot title
-		(default: "")
-	:param want_minor_grid:
-		Boolean, whether or not to plot minor gridlines
-		(default: False).
+	:param linewiths:
+		list of 2 floats, line widths for median and +/- epsilon curves
+		(default: None)
 	:param legend_location:
-		Integer, location of legend (matplotlib location code):
-		"best" 	0
-		"upper right" 	1
-		"upper left" 	2
-		"lower left" 	3
-		"lower right" 	4
-		"right" 	5
-		"center left" 	6
-		"center right" 	7
-		"lower center" 	8
-		"upper center" 	9
-		"center" 	10
+		int or str, location of legend (matplotlib location code):
+			"best" 	0
+			"upper right" 	1
+			"upper left" 	2
+			"lower left" 	3
+			"lower right" 	4
+			"right" 	5
+			"center left" 	6
+			"center right" 	7
+			"lower center" 	8
+			"upper center" 	9
+			"center" 	10
 		(default: 0)
 	:param lang:
 		str, shorthand for language of annotations.
 		Currently only "en" and "nl" are supported
-		(default: "en").
+		(default: "en")
+	:param title:
+		str, plot title
+		If None, a default title will be shown
+		(default: "")
+	:param fig_filespec:
+		str, full path specification of output file
+		(default: None)
+	:param kwargs:
+		additional keyword arguments understood by :func:`generic_mpl.plot_xy`
+
+	:return:
+		matplotlib Axes instances
 	"""
 	from .base import convert_distance_metric
 
-	linestyles = ("", "--", ":", "-.")
-	if not colors:
-		colors = ("k", "r", "g", "b", "c", "m", "y")
+	COLORS = colors or pylab.rcParams['axes.prop_cycle'].by_key()['color']
+	LINESTYLES = linestyles or ("-", "--", ":", "-.")
+	LINEWIDTHS = linewidths or [3, 1]
+	assert len(LINEWIDTHS) == 2
 
-	if plot_style.lower() in ("lin", "linlin"):
-		plotfunc = pylab.plot
-	elif plot_style.lower() == "linlog":
-		plotfunc = pylab.semilogy
-	elif plot_style.lower() == "loglin":
-		plotfunc = pylab.semilogx
-	elif plot_style.lower() == "loglog":
-		plotfunc = pylab.loglog
-
+	datasets, colors, linestyles, linewidths, labels = [], [], [], [], []
 	for i, gmpe in enumerate(gmpe_list):
+		num_dist = 50
 		if dmin is None:
 			dmin = gmpe.dmin
 		if dmax is None:
 			dmax = gmpe.dmax
 		## Avoid math domain errors with 0
-		if dmin == 0:
-			dmin = 0.1
-		distances = logrange(max(dmin, gmpe.dmin), min(dmax, gmpe.dmax), 25)
+		if xscaling == 'log':
+			if dmin == 0:
+				dmin = 0.1
+			distances = logrange(max(dmin, gmpe.dmin), min(dmax, gmpe.dmax), num_dist)
+		else:
+			distances = np.linspace(max(dmin, gmpe.dmin), min(dmax, gmpe.dmax), num_dist)
+
 		for j, M in enumerate(mags):
 			converted_distances = convert_distance_metric(distances,
 									gmpe.distance_metric, distance_metric, M)
 			Avalues = gmpe(M, converted_distances, h=h, imt=imt, T=T,
 							imt_unit=imt_unit, soil_type=soil_type, vs30=vs30,
 							kappa=kappa, mechanism=mechanism, damping=damping)
-			style = colors[i%len(colors)] + linestyles[j%len(linestyles)]
-			plotfunc(distances, Avalues, style, linewidth=3,
-					label=gmpe.name+" (M=%.1f)" % M)
+
+			datasets.append((converted_distances, Avalues))
+			color = COLORS[i%len(COLORS)]
+			colors.append(color)
+			linestyle = LINESTYLES[j%len(LINESTYLES)]
+			linestyles.append(linestyle)
+			linewidths.append(LINEWIDTHS[0])
+			labels.append(gmpe.name+" (M=%.1f)" % M)
+
 			if epsilon:
+				linewidth = LINEWIDTHS[1]
 				## Fortunately, log_sigma is independent of scale factor!
 				## Thus, the following are equivalent:
 				#log_sigma = gmpe.log_sigma(M, imt=imt, T=T, soil_type=soil_type, vs30=vs30, kappa=kappa, mechanism=mechanism, damping=damping)
@@ -158,55 +178,56 @@ def plot_distance(gmpe_list, mags, dmin=None, dmax=None, distance_metric=None,
 									imt_unit=imt_unit, epsilon=epsilon,
 									soil_type=soil_type, vs30=vs30, kappa=kappa,
 									mechanism=mechanism, damping=damping)
-				plotfunc(distances, Asigmavalues, style, linewidth=1,
-						label=gmpe.name+" (M=%.1f) $\pm %d \sigma$" % (M, epsilon))
+				datasets.append((converted_distances, Asigmavalues))
+				colors.append(color)
+				linestyles.append(linestyle)
+				linewidths.append(linewidth)
+				labels.append(gmpe.name+" (M=%.1f) $\pm %s \sigma$" % (M, epsilon))
+
 				#Asigmavalues = 10**(np.log10(Avalues) - log_sigma)
 				Asigmavalues = gmpe(M, converted_distances, h=h, imt=imt, T=T,
 									imt_unit=imt_unit, epsilon=-epsilon,
 									soil_type=soil_type, vs30=vs30, kappa=kappa,
 									mechanism=mechanism, damping=damping)
-				plotfunc(distances, Asigmavalues, style, linewidth=1, label='_nolegend_')
+				datasets.append((converted_distances, Asigmavalues))
+				colors.append(color)
+				linestyles.append(linestyle)
+				linewidths.append(linewidth)
+				labels.append('_nolegend_')
 
 	## Plot decoration
-	if distance_metric:
-		#pylab.xlabel(" ".join([distance_metric, "distance (km)"]), fontsize="x-large")
-		distance_metrics = [distance_metric]
-	else:
-		distance_metrics = set()
-		for gmpe in gmpe_list:
-			distance_metrics.add(gmpe.distance_metric)
-		distance_metrics = list(distance_metrics)
-	if len(distance_metrics) > 1:
-		pylab.xlabel(get_distance_label(None, lang), fontsize="x-large")
-	else:
-		pylab.xlabel(get_distance_label(distance_metrics[0], lang), fontsize="x-large")
-	imt_label = (get_imt_label(imt, lang.lower())
+	xlabel = kwargs.pop('xlabel', None)
+	if xlabel is None:
+		if distance_metric:
+			distance_metrics = [distance_metric]
+		else:
+			distance_metrics = set()
+			for gmpe in gmpe_list:
+				distance_metrics.add(gmpe.distance_metric)
+			distance_metrics = list(distance_metrics)
+		if len(distance_metrics) > 1:
+			xlabel = get_distance_label(None, lang)
+		else:
+			xlabel = get_distance_label(distance_metrics[0], lang)
+
+	ylabel = kwargs.pop('ylabel', None)
+	if ylabel is None:
+		ylabel = (get_imt_label(imt, lang.lower())
 				+ " (%s)" % IMT_UNIT_TO_PLOT_LABEL.get(imt_unit, imt_unit))
-	pylab.ylabel(imt_label, fontsize="x-large")
-	pylab.grid(True)
-	if want_minor_grid:
-		pylab.grid(True, which="minor")
+
 	if title is None:
 		title = "%s" % imt
 		if not imt in ("PGA", "PGV", "PGD"):
 			title += " (T=%s s)" % T
-	pylab.title(title)
-	font = FontProperties(size='medium')
-	pylab.legend(loc=legend_location, prop=font)
-	xmin, xmax, ymin, ymax = pylab.axis()
-	if amin is None:
-		amin = ymin
-	if amax is None:
-		amax = ymax
-	pylab.axis((dmin, dmax, amin, amax))
-	ax = pylab.gca()
-	for label in ax.get_xticklabels() + ax.get_yticklabels():
-		label.set_size('large')
-	if fig_filespec:
-		pylab.savefig(fig_filespec, dpi=300)
-		pylab.clf()
-	else:
-		pylab.show()
+
+	kwargs['xmin'] = kwargs.get('xmin', dmin)
+	kwargs['xmax'] = kwargs.get('xmax', dmax)
+
+	return plot_xy(datasets, labels=labels, colors=colors, linestyles=linestyles,
+					linewidths=linewidths, xscaling=xscaling, yscaling=yscaling,
+					xlabel=xlabel, ylabel=ylabel, xgrid=xgrid, ygrid=xgrid,
+					legend_location=legend_location, title=title,
+					fig_filespec=fig_filespec, **kwargs)
 
 
 def plot_spectrum(gmpe_list, mags, d, h=0, imt="SA", Tmin=None, Tmax=None,
