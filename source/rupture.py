cfb54826ac5dbd7ccf6c723a2e8e0b628f0ad73f
krisvanneste <kris.vanneste@oma.be>
2020-03-24 11:14:31 +0000

    PY2/3 compatibility changes. Added __repr__ method to Rupture. Allowed 'msr' argument of from_hypocenter method of Rupture to be string.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/hazard/rshalib/trunk@6242 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- source/rupture.py
+++ source/rupture.py
@@ -1,21 +1,45 @@
 """
+Ruptures
 """
 
+from __future__ import absolute_import, division, print_function, unicode_literals
 
-import openquake.hazardlib as oqhazlib
+try:
+	## Python 2
+	basestring
+except:
+	## Python 3
+	basestring = str
+
+
+from .. import oqhazlib
 
 from ..geo import NodalPlane, Point
 from ..pmf import HypocentralDepthDistribution, NodalPlaneDistribution
 
 
+
+__all__ = ['Rupture']
+
+
 class Rupture(oqhazlib.source.Rupture):
 	"""
 	"""
+	def __repr__(self):
+		txt = '<%s Rupture M=%.2f, rake=%.1f>'
+		txt %= (self.source_type, self.mag, self.rake)
+		return txt
+
+	@property
+	def source_type(self):
+		return self.source_typology.__name__[:-6]
 
 	@classmethod
-	def from_hypocenter(cls, lon, lat, depth, mag, strike, dip, rake, trt, rms, rar, usd, lsd, msr):
+	def from_hypocenter(cls, lon, lat, depth, mag, strike, dip, rake, trt,
+						rms, rar, usd, lsd, msr):
 		"""
-		Create rupture by extending hypocenter with magnitude scaling relationship.
+		Create rupture by extending hypocenter with magnitude scaling
+		relationship.
 
 		:params lon:
 			float, lon of rupture hypocenter
@@ -42,7 +66,7 @@ class Rupture(oqhazlib.source.Rupture):
 		:param lsd:
 			float, lower seismogenic depth
 		:param msr:
-			instance of subclass of :class:`openquake.hazardlib.scaler.BaseMSR`
+			str or instance of subclass of :class:`oqhazlib.scalerel.BaseMSR`
 
 		:return:
 			instance of :class:`Rupture`
@@ -51,8 +75,12 @@ class Rupture(oqhazlib.source.Rupture):
 		nodal_plane = NodalPlane(strike, dip, rake)
 		npd = NodalPlaneDistribution([nodal_plane], [1.])
 		hdd = HypocentralDepthDistribution([depth], [1.])
-		point_source = oqhazlib.source.PointSource("", "", trt, "", rms, msr, rar, usd, lsd, hypocenter, npd, hdd)
+		if (not isinstance(msr, oqhazlib.scalerel.BaseMSR)
+			and isinstance(msr, basestring)):
+			msr = getattr(oqhazlib.scalerel, msr)()
+		point_source = oqhazlib.source.PointSource("", "", trt, "", rms, msr,
+											rar, usd, lsd, hypocenter, npd, hdd)
 		surface = point_source._get_rupture_surface(mag, nodal_plane, hypocenter)
-		rupture = cls(mag, rake, trt, hypocenter, surface, source_typology=oqhazlib.source.PointSource)
+		rupture = cls(mag, rake, trt, hypocenter, surface,
+						source_typology=oqhazlib.source.PointSource)
 		return rupture
-
