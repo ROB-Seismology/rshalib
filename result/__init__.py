f0b64211901dafa1277f1f9603209b05caca5721
krisvanneste <kris.vanneste@oma.be>
2020-03-25 14:58:10 +0000

    Improved reloading mechanism.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/hazard/rshalib/trunk@6266 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- result/__init__.py
+++ result/__init__.py
@@ -1,22 +1,46 @@
-#
-# Empty file necessary for python to recognise directory as package
-#
+"""
+result submodule
+"""
 
-import plot
-reload(plot)
+from __future__ import absolute_import, division, print_function, unicode_literals
 
-import hazard_curve
-reload(hazard_curve)
 
-import deaggregation
-reload(deaggregation)
 
-from plot import (plot_hazard_curve, plot_hazard_spectrum, plot_histogram,
+## Reloading mechanism
+try:
+	reloading
+except NameError:
+	## Module is imported for the first time
+	reloading = False
+else:
+	## Module is reloaded
+	reloading = True
+	try:
+		## Python 3
+		from importlib import reload
+	except ImportError:
+		## Python 2
+		pass
+
+
+if not reloading:
+	from . import plot
+else:
+	reload(plot)
+from .plot import (plot_hazard_curve, plot_hazard_spectrum, plot_histogram,
 					plot_deaggregation)
 
-from hazard_curve import *
+if not reloading:
+	from . import hazard_curve
+else:
+	reload(hazard_curve)
+from .hazard_curve import *
 
-from deaggregation import (ExceedanceRateMatrix, ProbabilityMatrix,
+if not reloading:
+	from . import deaggregation
+else:
+	reload(deaggregation)
+from .deaggregation import (ExceedanceRateMatrix, ProbabilityMatrix,
 	FractionalContributionMatrix, DeaggregationSlice, DeaggregationCurve,
 	SpectralDeaggregationCurve, get_mean_deaggregation_slice,
 	get_mean_deaggregation_curve)
