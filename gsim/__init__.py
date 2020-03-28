3e6f85291ba008aea88d0933df29428791ec9c7c
krisvanneste <kris.vanneste@oma.be>
2020-03-28 17:32:29 +0000

    Removed obsolete code.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/hazard/rshalib/trunk@6300 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- gsim/__init__.py
+++ gsim/__init__.py
@@ -40,15 +40,3 @@ if not reloading:
 else:
 	reload(gmpe)
 from .gmpe import *
-
-"""
-from .gmpe import (CRISIS_DistanceMetrics, sd2psa, sd2psv, GMPE, AmbraseysEtAl1996GMPE, BergeThierry2003GMPE,
-	CauzziFaccioli2008GMPE, AkkarBommer2010GMPE, BommerEtAl2011GMPE, AkkarBommer2010SHAREGMPE,
-	McGuire1974GMPE, AbrahamsonSilva2008, AkkarBommer2010, AkkarEtAl2013, AtkinsonBoore2006, AtkinsonBoore2006Prime,
-	BindiEtAl2011, BooreAtkinson2008, BooreAtkinson2008Prime, Campbell2003, Campbell2003SHARE, Campbell2003adjusted,
-	CauzziFaccioli2008, ChiouYoungs2008, FaccioliEtAl2010, FaccioliEtAl2010Ext, ToroEtAl2002,
-	ToroEtAl2002SHARE, ToroEtAl2002adjusted, ZhaoEtAl2006Asc, RietbrockEtAl2013SS, RietbrockEtAl2013MD,
-	Campbell2003HTT, ToroEtAl2002HTT, RietbrockEtAl2013MDHTT, NhlibGMPE,
-	PezeshkEtAl2011, Anbazhagan2013, Atkinson2015, adjust_hard_rock_to_rock, adjust_host_to_target,
-	adjust_faulting_style, adjust_components, plot_distance, plot_spectrum)
-"""
