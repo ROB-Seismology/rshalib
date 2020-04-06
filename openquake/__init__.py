d9f2bb67daf89cfa8cf914d2623cd0517261e704
krisvanneste <kris.vanneste@oma.be>
2020-04-06 14:11:48 +0000

    Improved reloading mechanism.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/hazard/rshalib/trunk@6365 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- openquake/__init__.py
+++ openquake/__init__.py
@@ -1,20 +1,40 @@
-#
-# Empty file necessary for python to recognise directory as package
-#
+"""
+OpenQuake-specific input and output
+"""
 
-import IO
-reload(IO)
+from __future__ import absolute_import, division, print_function, unicode_literals
 
-import config
-## Reloading config may in some cases result in the following error:
-## super(OQ_Params, self).__init__(ini_filespec, configspec=configspec, write_empty_values=True, list_values=False)
-## TypeError: super(type, obj): obj must be an instance or subtype of type
-reload(config)
 
-from IO import (parse_hazard_curves, parse_hazard_curves_multi, parse_hazard_map,
-				parse_uh_spectra, parse_disaggregation, parse_disaggregation_full,
-				parse_any_output, read_curve_folder, read_multi_folder, read_shcft,
-				read_uhsft, parse_spectral_deaggregation_curve, parse_spectral_hazard_curve_field,
-				write_disaggregation_slice)
 
-from config import OQ_Params
+## Reloading mechanism
+try:
+	reloading
+except NameError:
+	## Module is imported for the first time
+	reloading = False
+else:
+	## Module is reloaded
+	reloading = True
+	try:
+		## Python 3
+		from importlib import reload
+	except ImportError:
+		## Python 2
+		pass
+
+
+
+if not reloading:
+	from . import config
+else:
+	## Reloading config may in some cases result in the following error:
+	## super(OQ_Params, self).__init__(ini_filespec, configspec=configspec, write_empty_values=True, list_values=False)
+	## TypeError: super(type, obj): obj must be an instance or subtype of type
+	reload(config)
+from .config import *
+
+if not reloading:
+	from . import IO
+else:
+	reload(IO)
+from .IO import *
