fd2035f67eb2e7aa55af2e147ab09fd01b7c2ccd
krisvanneste <kris.vanneste@oma.be>
2020-04-10 20:15:05 +0000

    Added missing import.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/hazard/rshalib/trunk@6479 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- result/deaggregation.py
+++ result/deaggregation.py
@@ -4,6 +4,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from decimal import Decimal
+from lxml import etree
 import numpy as np
 
 import openquake.hazardlib.calc.disagg as disagg
