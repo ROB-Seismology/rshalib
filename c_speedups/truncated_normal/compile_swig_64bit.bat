658388989a3e962e266fe4d7dc5ab8a904636efd
krisvanneste <kris.vanneste@oma.be>
2020-11-24 09:34:06 +0000

    Updated compile_swig_64bit.bat script.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/hazard/rshalib/trunk@7206 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- c_speedups/truncated_normal/compile_swig_64bit.bat
+++ c_speedups/truncated_normal/compile_swig_64bit.bat
@@ -1,6 +1,6 @@
 swig -python truncated_normal.i
 
-"C:\Anaconda\Scripts\gcc.bat" -DMS_WIN64 -mdll -O2 -Wall -IC:\Anaconda\include -IC:\Anaconda\Lib\site-packages\numpy\core\include -LC:\Anaconda\libs truncated_normal.c truncated_normal_wrap.c -lpython27 -o _truncated_normal.pyd
+gcc -DMS_WIN64 -mdll -O2 -Wall -IC:\Miniconda3\envs\py2\include -IC:\Miniconda3\envs\py2\Lib\site-packages\numpy\core\include -LC:\Miniconda3\envs\py2\libs truncated_normal.c truncated_normal_wrap.c -lpython27 -o _truncated_normal.pyd
 
 In Mingw64 environment:
-gcc -DMS_WIN64 -mdll -O2 -Wall -I/C/Anaconda/envs/py3/include -I/C/Anaconda/env s/py3/Lib/site-packages/numpy/core/include -L/C/Anaconda/envs/py3 truncated_normal.c truncated_norm al_wrap.c -lpython36 -o _truncated_normal.pyd
+gcc -DMS_WIN64 -mdll -O2 -Wall -I/C/Miniconda3/envs/py3/include -I/C/Miniconda3/envs/py3/Lib/site-packages/numpy/core/include -L/C/Miniconda3/envs/py3 truncated_normal.c truncated_normal_wrap.c -lpython37 -o _truncated_normal.cp37-win_amd64.pyd
