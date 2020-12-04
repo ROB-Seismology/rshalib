841770d6963204ffb53315acc51e194d7f7a8ebf
krisvanneste <kris.vanneste@oma.be>
2020-12-04 17:21:57 +0000

    Added 'importance_class' argument to get_ec8_rs function. Added comment concerning vertical response spectra for soil types other than A.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/hazard/rshalib/trunk@7229 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- refspec/en1998.py
+++ refspec/en1998.py
@@ -12,14 +12,14 @@ from ..result import ResponseSpectrum
 __all__ = ['get_ec8_rs']
 
 
-def get_ec8_rs(ag, ground_type, resp_type, orientation="horizontal",
-					damping=5):
+def get_ec8_rs(agr, ground_type, resp_type, orientation="horizontal",
+					importance_class=2, damping=5):
 	"""
 	Reference horizontal elastic response spectrum according to Eurocode 8
 	(if deep geology is not accounted for)
 
-	:param ag:
-		float, design ground acceleration on type A ground (in g)
+	:param agr:
+		float, reference ground acceleration on type A ground (in g)
 	:param ground_type:
 		str, one of the ground types defined in Eurocode 8: A, B, C, D or E
 	:param resp_type:
@@ -31,6 +31,9 @@ def get_ec8_rs(ag, ground_type, resp_type, orientation="horizontal",
 	:param orientation:
 		str, either "horizontal" or "vertical", orientation of spectrum
 		(default: "horizontal")
+	:param importance_class:
+		int, importance class of building (1, 2, 3 or 4)
+		(default: 2)
 	:param damping:
 		float, viscous damping, in %
 		(default: 5)
@@ -38,10 +41,13 @@ def get_ec8_rs(ag, ground_type, resp_type, orientation="horizontal",
 	:return:
 		instance of :class:`rshalib.result.ResponseSpectrum`
 	"""
+	importance_factor = {1: 0.8, 2: 1., 3: 1.2, 4: 1.4}[importance_class]
+	ag = agr * importance_factor
+
 	ground_type = ground_type.upper()
 	if resp_type == 1:
+		S = {'A': 1.0, 'B': 1.2, 'C': 1.15, 'D': 1.35, 'E': 1.4}[ground_type]
 		if orientation == "horizontal":
-			S = {'A': 1.0, 'B': 1.2, 'C': 1.15, 'D': 1.35, 'E': 1.4}[ground_type]
 			TB = {'A': 0.15, 'B': 0.15, 'C': 0.2, 'D': 0.2, 'E': 0.15}[ground_type]
 			TC = {'A': 0.4, 'B': 0.5, 'C': 0.6, 'D': 0.8, 'E': 0.5}[ground_type]
 			TD = {'A': 2.0, 'B': 2.0, 'C': 2.0, 'D': 2.0, 'E': 2.0}[ground_type]
@@ -49,8 +55,8 @@ def get_ec8_rs(ag, ground_type, resp_type, orientation="horizontal",
 			vh_ratio = 0.9
 			TB, TC, TD = 0.05, 0.15, 1.  ## Is this correct for type 1 ??
 	elif resp_type == 2:
+		S = {'A': 1.0, 'B': 1.35, 'C': 1.5, 'D': 1.8, 'E': 1.6}[ground_type]
 		if orientation == "horizontal":
-			S = {'A': 1.0, 'B': 1.35, 'C': 1.5, 'D': 1.8, 'E': 1.6}[ground_type]
 			TB = {'A': 0.05, 'B': 0.05, 'C': 0.1, 'D': 0.1, 'E': 0.05}[ground_type]
 			TC = {'A': 0.25, 'B': 0.25, 'C': 0.25, 'D': 0.30, 'E': 0.25}[ground_type]
 			TD = {'A': 1.2, 'B': 1.2, 'C': 1.2, 'D': 1.2, 'E': 1.2}[ground_type]
@@ -74,6 +80,11 @@ def get_ec8_rs(ag, ground_type, resp_type, orientation="horizontal",
 			elif T >= TD:
 				values[i] = ag * S * eta * 2.5 * ((TC*TD)/T**2)
 	elif orientation == "vertical":
+		## Note: equations in EN-1998 do not contain S parameter for vertical
+		## spectra
+		## Even though the formulae are similar to those for horizontal spectra,
+		## it seems better to multiply horizontal spectra for other soil types
+		## with the V/H spectral ratio computed for class A
 		assert ground_type.upper() == "A"
 		avg = ag * vh_ratio
 		for i, T in enumerate(periods):
